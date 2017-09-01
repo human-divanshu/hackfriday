d94ec71412aceff1061b6dfa008598e49c77d97a
DSingh <human.divanshu@gmail.com>
2017-09-02 02:12:12 +0530

    Added decleration for shell functions

--- cmd.h
+++ cmd.h
@@ -4,16 +4,29 @@
 
 using namespace std;
 
-class Command {
-	public:
-		string cmdName;
-		int cmdArg;
-
-	Command(string s, int i) {
-		cmdName = s;
-		cmdArg = i;		
-	}
-};
-
-vector<Command> cmdVector;
-void cmdinit();
+void copy(string path1, string path2);
+void dir();
+void pwd();
+void cd(string path);
+void move(string file1, string des);
+void find(string filename);
+void whoami();
+void echo(string s);
+void history();
+void help();
+void cat(string filename);
+void wc(string filename);
+void date();
+void date();
+void diff(string file1, string file2);
+void head(string filename);
+void tail(string filename);
+void man(string cmd);
+void time();
+void del(string filename);
+void rename(string filename, string newname);
+void sort(string filename); // sort contents
+void unique(string filename); // arrage content 
+void sleep(int timesec);
+void uptime();
+void redirect(string content, string filename); // write content to file
